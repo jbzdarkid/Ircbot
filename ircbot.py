fa0cbd30ce5f04170374894b5d55723b86aa1853
jbzdarkid <jbzdarkid@gmail.com>
2018-06-27 18:22:57 -0700

    Probably goodness

--- ircbot.py
+++ ircbot.py
@@ -3,45 +3,15 @@ from re import compile
 from select import select
 import threading
 import requests
-from config import client_id, secret
+from config import chat_token, username
 
 from Tkinter import *
 
-username = 'jbzdarkid'
 CHAT_MSG = compile('^:(\w+)!\w+@\w+.tmi.twitch.tv PRIVMSG #' + username + ' :(.*)\r\n$')
-
-"""
-class Popup(object):
-  def __init__(self,master):
-    top = self.top = Toplevel(master)
-    self.label = Label(top, text="Hello World")
-    self.label.pack()
-    self.entry = Entry(top)
-    self.entry.pack()
-    self.button = Button(top, text='Ok', command=self.cleanup)
-    self.button.pack()
-
-  def cleanup(self):
-    self.value = self.entry.get()
-    self.top.destroy()
-    
-"""
-
-def twitch_api_get(query=None):
-  url = 'https://api.twitch.tv/kraken'
-  if query:
-    url += '/' + query
-  return requests.get(
-    url,
-    headers={
-      'Authorization': 'OAuth ' + token,
-      'x-api-version': '5',
-    },
-  ).json()
 
 def on_chat(username, message):
   print(username+': '+message)
-  
+
   if message.startswith('!'):
     message_parts = message[1:].split(' ')
     if message_parts[0] in COMMANDS:
@@ -52,7 +22,7 @@ def votekick(username, *args):
 
 def ping(*args):
   send_message('pong')
-  
+
 def commands(*args):
   command_list = COMMANDS.keys()
   command_list.sort()
@@ -68,14 +38,14 @@ def start_ui():
   root = Tk()
   root.title('Ircbot')
   root.geometry('500x500')
-  
+
   title = Button(root)
   title['text'] = "QUIT"
   title['fg'] = "red"
   title['command'] = ping
   title.pack({"side": "left"})
 
-  
+
   root.mainloop()
   root.destroy()
 
@@ -87,7 +57,7 @@ if __name__ == '__main__':
   # Connect to twitch
   irc = socket()
   irc.connect(('irc.chat.twitch.tv', 6667))
-  irc.send('PASS oauth:' + token + '\r\n')
+  irc.send('PASS oauth:' + chat_token + '\r\n')
   irc.send('NICK ' + username + '\r\n')
   data = irc.recv(4096) # You are in a maze of twisty passages
 
@@ -95,7 +65,7 @@ if __name__ == '__main__':
   data = irc.recv(1024) # /JOIN
   data = irc.recv(1024) # End of /NAMES list
   irc.setblocking(0)
-  
+
   while 1:
     # Wait for new data so that control-C works
     ready = select([irc], [], [], 1) # 1 second timeout
@@ -110,7 +80,8 @@ if __name__ == '__main__':
         continue
       on_chat(m.group(1), m.group(2))
 
+  def send_whisper(username, message):
+    irc.send('PRIVMSG #jtv :/w '+username+' '+message+'\r\n')
+
   def send_message(message):
     irc.send('PRIVMSG #'+username+' :'+message+'\r\n')
-  
-
